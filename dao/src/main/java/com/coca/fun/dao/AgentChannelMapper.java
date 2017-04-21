6a4dc40eb87eccdb70853806a610ac8d168f7590
shikang.y@gmail.com <shikang.y@gmail.com>
2017-04-21 11:39:07 +0800

    数据库

---
+++ dao/src/main/java/com/coca/fun/dao/AgentChannelMapper.java
@@ -0,0 +1,7 @@
+package com.coca.fun.dao;
+
+import com.coca.fun.domain.AgentChannel;
+import com.coca.fun.utils.MyMapper;
+
+public interface AgentChannelMapper extends MyMapper<AgentChannel> {
+}
---
+++ dao/src/main/java/com/coca/fun/dao/AgentMapper.java
@@ -0,0 +1,7 @@
+package com.coca.fun.dao;
+
+import com.coca.fun.domain.Agent;
+import com.coca.fun.utils.MyMapper;
+
+public interface AgentMapper extends MyMapper<Agent> {
+}
