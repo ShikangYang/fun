a22b50f71d258fb3a4b1c2563804bfede19046f1
shikang.y@gmail.com <shikang.y@gmail.com>
2017-04-21 15:49:48 +0800

    filter 添加

--- service/src/main/java/com/coca/fun/service/filter/Filter.java
+++
@@ -1,7 +0,0 @@
-package com.coca.fun.service.filter;
-
-/**
- * Created by yangshikang on 2017/4/21.
- */
-public class Filter {
-}
--- start/src/main/java/com/coca/fun/start/Application.java
+++ start/src/main/java/com/coca/fun/start/Application.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @MapperScan(basePackages = "com.coca.fun.dao")
 @ComponentScan(basePackages = "com.coca.fun.rest")
+@ServletComponentScan(value = {"com.coca.fun.service.filter"})
 public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
