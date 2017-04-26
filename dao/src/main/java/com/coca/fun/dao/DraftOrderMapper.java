2a3b6285617c0d85a3cc81bf798e5c9781e59421
shikang.y@gmail.com <shikang.y@gmail.com>
2017-04-26 10:43:27 +0800

    草稿表添加

---
+++ dao/src/main/java/com/coca/fun/dao/DraftOrderMapper.java
@@ -0,0 +1,7 @@
+package com.coca.fun.dao;
+
+import com.coca.fun.dao.domain.DraftOrder;
+import com.coca.fun.service.utils.MyMapper;
+
+public interface DraftOrderMapper extends MyMapper<DraftOrder> {
+}
