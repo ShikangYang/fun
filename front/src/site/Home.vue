bb991995cc237ccf2671644a87be790805499090
sunindexs <1219526576@qq.com>
2017-11-14 16:24:51 +0800

    测试首页。。

--- front/src/site/Home.vue
+++ front/src/site/Home.vue
@@ -16,14 +16,7 @@
                 </div>
                 </el-col>
             </el-row>
-            <el-menu
-                    :default-active="activeIndex2"
-                    class="el-menu-demo"
-                    mode="horizontal"
-                    @select="handleSelect"
-                    background-color="#545c64"
-                    text-color="#fff"
-                    active-text-color="#ffd04b">
+            <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
                 <el-menu-item index="1">处理中心</el-menu-item>
                 <el-submenu index="2">
                     <template slot="title">我的工作台</template>
@@ -33,100 +26,49 @@
                 </el-submenu>
                 <el-menu-item index="3"><a href="#" target="_blank">订单管理</a></el-menu-item>
             </el-menu>
+            <div class="line"></div>
         </header>
         <main class="el-main">
-            <el-breadcrumb separator-class="el-icon-arrow-right">
-                <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
-                <el-breadcrumb-item>活动管理</el-breadcrumb-item>
-                <el-breadcrumb-item>活动列表</el-breadcrumb-item>
-                <el-breadcrumb-item>活动详情</el-breadcrumb-item>
-            </el-breadcrumb>
-            <el-collapse @change="handleChange">
-                <el-collapse-item title="一致性 Consistency" name="1">
-                    <div><el-tag>标签一</el-tag>
-                        <el-tag type="success">标签二</el-tag>
-                        <el-tag type="info">标签三</el-tag>
-                        <el-tag type="warning">标签四</el-tag>
-                        <el-tag type="danger">标签五
-                        </el-tag></div>
-                </el-collapse-item>
-                <el-collapse-item title="反馈 Feedback" name="2">
-                    <div>控制反馈：通过界面样式和交互动效让用户可以清晰的感知自己的操作；</div>
-                    <div>页面反馈：操作后，通过页面元素的变化清晰地展现当前状态。</div>
-                </el-collapse-item>
-            </el-collapse>
 
-            <el-tag
-                    v-for="tag in tags"
-                    :key="tag"
-                    closable
-                    :disable-transitions="false"
-                    @close="handleClose(tag)">
-                {{tag}}
-            </el-tag>
-            <el-table
-                    ref="singleTable"
-                    :data="tableData"
-                    highlight-current-row
-                    @current-change="handleCurrentChange"
-                    style="width: 100%">
-                <el-table-column
-                        type="index"
-                        width="50">
-                </el-table-column>
-                <el-table-column
-                        property="date"
-                        label="日期"
-                        width="120">
-                </el-table-column>
-                <el-table-column
-                        property="name"
-                        label="姓名"
-                        width="120">
-                </el-table-column>
-                <el-table-column
-                        property="address"
-                        label="地址">
-                </el-table-column>
-            </el-table>
-            <div class="block">
-                <el-pagination
-                        @current-change="handleCurrentChange"
-                        :page-size="10"
-                        layout="total, prev, pager, next"
-                        :total="30">
-                </el-pagination>
-            </div>
+            <el-carousel indicator-position="outside">
+                <el-carousel-item v-for="item in 4" :key="item">
+                    <h3>{{ item }}</h3>
+                </el-carousel-item>
+            </el-carousel>
+            <el-row>
+                <el-col :span="6" v-for="(o, index) in 3" :key="o" :offset="index > 0 ? 3 : 0">
+                    <el-card :body-style="{ padding: '0px' }">
+                        <img src="http://element-cn.eleme.io/static/hamburger.50e4091.png" class="image">
+                        <div style="padding: 14px;">
+                            <span>好吃的汉堡</span>
+                            <div class="bottom clearfix">
+                                <time class="time">{{ currentDate }}</time>
+                                <el-button type="text" class="button">操作按钮</el-button>
+                            </div>
+                        </div>
+                    </el-card>
+                </el-col>
+            </el-row>
+            <!--<div>-->
+                <!--<el-button @click="Login">登录</el-button>-->
+                <!--<el-button type="primary">主要按钮</el-button>-->
+                <!--<el-button type="success">成功按钮</el-button>-->
+                <!--<el-button type="info">信息按钮</el-button>-->
+                <!--<el-button type="warning">警告按钮</el-button>-->
+                <!--<el-button type="danger">危险按钮</el-button>-->
+            <!--</div>-->
         </main>
         <footer class="el-footer" style="height: 60px;">Footer</footer>
     </div>
-
 </template>
 
 <script>
-
     export default {
         name: 'Home',
         data: function() {
-            return { visible: false,activeIndex2: '1',
-                tags: ['标签一', '标签二', '标签三3333'],
-                tableData: [{
-                    date: '2016-05-02',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1518 弄'
-                }, {
-                    date: '2016-05-04',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1517 弄'
-                }, {
-                    date: '2016-05-01',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1519 弄'
-                }, {
-                    date: '2016-05-03',
-                    name: '王小虎',
-                    address: '上海市普陀区金沙江路 1516 弄'
-                }]
+            return {
+                num1: 1,
+                currentDate: new Date()
             }
         },
         methods: {
@@ -144,6 +86,12 @@
             },
             handleCurrentChange(){
                 alert();
+            },
+            handleChange(value) {
+                console.log(value);
+            },
+            Login(){
+                Login;
             }
         }
     }
@@ -160,5 +108,49 @@
     }
     .el-breadcrumb{margin-bottom: 15px;}
     .el-pagination{margin-top: 10px;}
+    .el-carousel__item h3 {
+        color: #475669;
+        font-size: 18px;
+        opacity: 0.75;
+        line-height: 300px;
+        margin: 0;
+    }
+
+    .el-carousel__item:nth-child(2n) {
+        background-color: #99a9bf;
+    }
+
+    .el-carousel__item:nth-child(2n+1) {
+        background-color: #d3dce6;
+    }
+    .time {
+        font-size: 13px;
+        color: #999;
+    }
+
+    .bottom {
+        margin-top: 13px;
+        line-height: 12px;
+    }
+
+    .button {
+        padding: 0;
+        float: right;
+    }
+
+    .image {
+        width: 100%;
+        display: block;
+    }
+
+    .clearfix:before,
+    .clearfix:after {
+        display: table;
+        content: "";
+    }
+
+    .clearfix:after {
+        clear: both
+    }
 </style>
 
